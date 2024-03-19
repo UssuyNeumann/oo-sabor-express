68c935567fa7f436e22e0bc89cfb5f6f36a8a99a
UssuyNeumann <luis.eduardo.un@hotmail.com>
2024-03-18 22:45:44 -0300

    utilizando metodo de classe e property um decorador que permite modificar atributos de uma classe

--- models/restaurante.py
+++ models/restaurante.py
@@ -1,19 +1,30 @@
 class Restaurante: 
-    restaurantes = []
+    restaurantes = [] 
+    # _nome atributo protegido e não totalmente privado
     def __init__(self, nome, categoria): 
-        self.nome = nome 
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome 
+        self._categoria = categoria
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self): 
         return f'{self.nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod #medo da classe
+    def listar_restaurantes(cls):
+        print(f"{'Nome'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property 
+    def ativo(self):
+        return '☒' if self._ativo else '☐'
+    
+    def alterar_status(self):
+        self._ativo = not self._ativo
 
 restaurante_pizza = Restaurante('Pizza','Pizzaria')
+restaurante_pizza.alterar_status()
 restaurante_sushi = Restaurante('Sushi','Oriental')
 
 Restaurante.listar_restaurantes()
