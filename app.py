91d72191e79829a6993d6a5c1fb52cbc6bfd3391
UssuyNeumann <luis.eduardo.un@hotmail.com>
2024-10-06 00:01:42 -0300

    Criando classe de avaliação, medo de receber avaliacao e usar  property para modificar o uso do atributo avaliacao

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from models.restaurante import Restaurante
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_mexicano = Restaurante('El Caliente', 'Mexicana')
-restaurante_japones = Restaurante('Sushi Ja', 'Oriental')
+# restaurante_mexicano = Restaurante('El Caliente', 'Mexicana')
+# restaurante_japones = Restaurante('Sushi Ja', 'Oriental')
 
-restaurante_mexicano.alterar_status()
+# restaurante_mexicano.alterar_status()
+
+restaurante_praca.receber_avaliacao('Luis',1)
+restaurante_praca.receber_avaliacao('Paula',8)
+restaurante_praca.receber_avaliacao('Michael',5)
 
 def main():
     Restaurante.listar_restaurantes()
--- models/restaurante.py
+++ models/restaurante.py
@@ -1,3 +1,9 @@
+
+
+
+from models.avaliacao import Avaliacao
+
+
 class Restaurante: 
     restaurantes = [] 
     # _nome atributo protegido e não totalmente privado
@@ -5,6 +11,7 @@ class Restaurante:
         self._nome = nome 
         self._categoria = categoria
         self._ativo = False
+        self._avaliacao = [] 
         Restaurante.restaurantes.append(self)
 
     def __str__(self): 
@@ -12,9 +19,9 @@ class Restaurante:
     
     @classmethod #medo da classe
     def listar_restaurantes(cls):
-        print(f"{'Nome'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
+        print(f"{'Nome'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}")
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property 
     def ativo(self):
@@ -22,3 +29,18 @@ class Restaurante:
     
     def alterar_status(self):
         self._ativo = not self._ativo
+
+    #método da avaliação e objeto avaliação que recebe os valores
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+    
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-' 
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_notas = len(self._avaliacao)
+        media = round(soma_notas/quantidade_notas, 1)
+        return media
